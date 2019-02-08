4946d704025afb4f05e0ddc644752b9bd48e586b
Trainee <Trainee@holistic-net.de>
2019-02-08 14:05:51 +0100

    small changes

---
+++ quiz_service.py
@@ -0,0 +1,50 @@
+from question_service import Questions
+import random
+
+class Quiz_Ablauf:
+    
+    global fragen_liste                                     
+    fragen_liste = [1,2,3,4,5,6,7,8,9,10]
+
+    def fragen_auswahl(self):                               
+        global ausgewählte_frage
+        ausgewählte_frage = random.randint(1,10)
+        anzahl_der_fragen = len(fragen_liste)
+        zähler = 0
+        for item in fragen_liste:                           
+            if ausgewählte_frage == fragen_liste[zähler]:  
+                fragen_liste.remove(ausgewählte_frage)
+            zähler += 1
+        if anzahl_der_fragen == len(fragen_liste):
+            fragen_auswahl()
+    
+    def start(self):
+        x = 1
+        score = 0
+        frage = Questions(0)
+        username = input("Spielername: ")
+        while x <= 10:
+            self.fragen_auswahl()
+            if ausgewählte_frage == 1:
+                score += frage.frage1()
+            if ausgewählte_frage == 2:
+                score += frage.frage2()
+            if ausgewählte_frage == 3:
+                score += frage.frage3()
+            if ausgewählte_frage == 4:
+                score += frage.frage4()
+            if ausgewählte_frage == 5:
+                score += frage.frage5()
+            if ausgewählte_frage == 6:
+                score += frage.frage6()
+            if ausgewählte_frage == 7:
+                score += frage.frage7()
+            if ausgewählte_frage == 8:
+                score += frage.frage9()
+            if ausgewählte_frage == 9:
+                score += frage.frage9()
+            if ausgewählte_frage == 10:
+                score += frage.frage10()
+            x += 1
+        
+    
